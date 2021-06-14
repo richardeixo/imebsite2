6be0154be8e6b0202e2e742a89cddb2063fde0e9
richardeixo <richard@eixo.digital>
2021-06-14 12:53:46 -0300

    Initial commit

---
+++ richard/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/header.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<img src="imagens/logo.png" />
+				<div class="tab-climed">
+					<ul>
+						<li><a href="#">Cliente</a></li>
+						<li><a href="#">Médico</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-6 top-right">
+				<div class="rsociais">
+					<a href="#"><div class="ico-fb"></div></a>
+					<a href="#"><div class="ico-ig"></div></a>
+					<a href="#"><div class="ico-in"></div></a>
+				</div>
+				<button type="button" class="btn btn-primary">AGENDE <br/>SEU EXAME</button>
+			</div>
+		</div>
+	</div>
