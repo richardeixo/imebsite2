8686f6a3c5045082db9c624a933acdcba550fd41
richardeixo <richard@eixo.digital>
2021-06-15 11:06:40 -0300

    exames/menu

--- homedesk/header.php
+++ homedesk/header.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 	<div class="container">
-		<div class="row">
+		<div class="row top-bar">
 			<div class="col-md-6 top-left">
 				<img src="imagens/logo.png" />
 				<div class="tab-climed">
@@ -23,7 +23,39 @@
 					<a href="#"><div class="ico-ig"></div></a>
 					<a href="#"><div class="ico-in"></div></a>
 				</div>
-				<button type="button" class="btn btn-primary">AGENDE <br/>SEU EXAME</button>
+				<button type="button" class="btn btn-primary btn-agende">AGENDE <br/>SEU EXAME</button>
 			</div>
 		</div>
 	</div>
+	<div class="container-fluid nav-bar">
+		<div class="container">
+			><a href="#" class="btn-exames"> Exames</a>
+			<nav class="menu">
+				<ul>
+					<li><a href="#">Convênios</a></li>
+					<li><a href="#">Resultado de Exames</a></li>
+					<li><a href="#">Unidades</a></li>
+					<li><a href="#">Cuide da sua Saúde</a></li>
+					<li><a href="#">Fale Conosco</a></li>
+					<li><a href="#">Materiais Gratuitos</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+
+<section class="exames">
+	<div class="container">
+		<h1>EXAMES</h1>
+		<div class="col-sm-6 img-exames">
+			<img src="imagens/exames/ecografia.png" />
+			<img src="imagens/exames/densitometria.png"  />
+			<img src="imagens/exames/pet.png"  />
+		</div>
+		<div class="col-sm-6 img-exames">
+			<img src="imagens/exames/cintilografia.png"  />
+			<img src="imagens/exames/mamografia.png"  />
+			<img src="imagens/exames/res-magnetica.png"  />
+		</div>
+		<br/>
+	</div>
+</section>
