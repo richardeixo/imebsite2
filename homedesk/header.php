b165f4b954d10128e1e05c495f86839524cb921e
richardeixo <richard@eixo.digital>
2021-06-16 23:23:09 -0300

    organização

--- homedesk/header.php
+++ homedesk/header.php
@@ -4,6 +4,9 @@
 	<title></title>
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="css/header.css">
+<link rel="stylesheet" type="text/css" href="css/style-exames.css">
+<link rel="stylesheet" type="text/css" href="css/style-content-blog.css">
+<link rel="stylesheet" type="text/css" href="css/style-newsletter.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script type="text/javascript">
 	var visibilidade = false;
@@ -17,6 +20,22 @@
             visibilidade = true;//Alteramos o valor da variável para true.
         }
     }
+    function cliente() {
+    	document.getElementById("menu-medico").style.display = "none";
+    	document.getElementById("menu-cliente").style.display = "block";
+    	document.getElementById("btn-cliente").classList.add("ativa");
+    	document.getElementById("btn-cliente").classList.remove("inativa");
+    	document.getElementById("btn-medico").classList.remove("ativa");
+    	document.getElementById("btn-medico").classList.add("inativa");
+    }
+    function medico() {
+    	document.getElementById("menu-medico").style.display = "block";
+    	document.getElementById("menu-cliente").style.display = "none";
+    	document.getElementById("btn-cliente").classList.add("inativa");
+    	document.getElementById("btn-cliente").classList.remove("ativa");
+    	document.getElementById("btn-medico").classList.remove("inativa");
+    	document.getElementById("btn-medico").classList.add("ativa");
+    }
 </script>
 </head>
 <body>
@@ -26,8 +45,8 @@
 				<img src="imagens/logo.png" />
 				<div class="tab-climed">
 					<ul>
-						<li><a href="#">Cliente</a></li>
-						<li><a href="#">Médico</a></li>
+						<li><button id="btn-cliente" class="ativa" onclick="cliente()">Cliente</button></li>
+						<li><button id="btn-medico" class="inativa" onclick="medico()">Médico</button></li>
 					</ul>
 				</div>
 			</div>
@@ -44,7 +63,8 @@
 	<div class="container-fluid nav-bar">
 		<div class="container">
 			><button class="btn-exames" onclick="ocultarExibir()"> Exames</button>
-			<nav class="menu">
+
+			<nav class="menu menu-cliente" id="menu-cliente">
 				<ul>
 					<li><a href="#">Convênios</a></li>
 					<li><a href="#">Resultado de Exames</a></li>
@@ -52,6 +72,14 @@
 					<li><a href="#">Cuide da sua Saúde</a></li>
 					<li><a href="#">Fale Conosco</a></li>
 					<li><a href="#">Materiais Gratuitos</a></li>
+				</ul>
+			</nav>
+
+			<nav class="menu menu-medico" id="menu-medico">
+				<ul>
+					<li><a href="#">Médico</a></li>
+					<li><a href="#">Médico 2</a></li>
+					<li><a href="#">Médico 3</a></li>
 				</ul>
 			</nav>
 			<div class="drop-exames" id="drop-exames">
@@ -136,17 +164,89 @@
 
 <section class="exames">
 	<div class="container">
-		<h1>EXAMES</h1>
-		<div class="col-sm-6 img-exames">
-			<img src="imagens/exames/ecografia.png" />
-			<img src="imagens/exames/densitometria.png"  />
-			<img src="imagens/exames/pet.png"  />
-		</div>
-		<div class="col-sm-6 img-exames">
-			<img src="imagens/exames/cintilografia.png"  />
-			<img src="imagens/exames/mamografia.png"  />
-			<img src="imagens/exames/res-magnetica.png"  />
+		<h1 class="titulo-exames">EXAMES</h1>
+		<div class="box-img-exames">
+			<img src="imagens/exames/ecografia.png" class="img-exames" />
+			<img src="imagens/exames/densitometria.png"  class="img-exames"/>
+			<img src="imagens/exames/pet.png"  class="img-exames"/>
+			<img src="imagens/exames/cintilografia.png"  class="img-exames"/>
+			<img src="imagens/exames/mamografia.png"  class="img-exames"/>
+			<img src="imagens/exames/res-magnetica.png"  class="img-exames"/>
+			<img src="imagens/exames/mamografia.png"  class="img-exames"/>
+			<img src="imagens/exames/res-magnetica.png"  class="img-exames"/>
 		</div>
 		<br/>
 	</div>
 </section>
+
+<section class="conteudo-blog-m">
+        <h2>CONTEÚDOS DA SAÚDE</h2>
+        <div class="container">
+            <div class="cards-conteudo">
+                <div class="card-conteudo">
+                    <div class="texto-conteudo">
+                        <h3>Título que pode conter uma linha apenas ou várias delas.</h3>
+                        <a href="#"><button>Ver mais</button></a>
+                    </div>
+                </div>
+
+                <div class="card-conteudo">
+                    <div class="texto-conteudo">
+                        <h3>Título que pode conter uma linha apenas ou várias delas.</h3>
+                        <a href="#"><button>Ver mais</button></a>
+                    </div>
+                </div>
+
+
+            </div>            
+        </div>
+
+
+        <div class="container">
+            <div class="cards-conteudo">
+                <div class="card-conteudo">
+                    <div class="texto-conteudo">
+                        <h3>Título que pode conter uma linha apenas ou várias delas.</h3>
+                        <a href="#"><button>Ver mais</button></a>
+                    </div>
+                </div>
+
+                <div class="card-conteudo">
+                    <div class="texto-conteudo">
+                        <h3>Título que pode conter uma linha apenas ou várias delas.</h3>
+                        <a href="#"><button>Ver mais</button></a>
+                    </div>
+                </div>
+
+            </div>
+
+            
+        </div>
+
+        <div class="container">
+            <div class="cards-conteudo">
+                <div class="card-conteudo-video">
+                    <iframe width="545" height="250" src="https://www.youtube.com/embed/P8A2iXI2JdM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+                <div class="card-conteudo-video">
+                    <iframe width="545" height="250" src="https://www.youtube.com/embed/P8A2iXI2JdM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+            
+        </div>
+
+    </section>
+
+    <section class="newsletter">
+    		<div class="coluna-txt">
+    			<h1>DICAS DE SAÚDE</h1>
+    			<span>Receba Dicas de Saúde Atualizadas</span>
+    			<input type="text" name="" placeholder="Digite seu email"><button type="button">Enviar</button>
+    		</div>
+    		<div class="coluna-img">
+    			<img src="imagens/img-news.png">
+    		</div>
+    </section>
