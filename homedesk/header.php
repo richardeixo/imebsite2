3261631cc8783247ad751c27afb4f3ac3eecc66d
richardeixo <richard@eixo.digital>
2021-07-07 15:52:46 -0300

    rodapé bug

--- homedesk/header.php
+++ homedesk/header.php
@@ -535,20 +535,9 @@ function closeNav() {
     	<div class="container espaco">
     		<div class="footer-info">  		
     			<img src="imagens/logo.png">
-    			<div class="f-rsociais">
-					<a href="#"><div class="f-ico-fb"></div></a>
-					<a href="#"><div class="f-ico-ig"></div></a>
-					<a href="#"><div class="f-ico-in"></div></a>
-				</div>
-				<h1>CENTRAL DE ATENDIMENTO</h1>
-				<span>(61) 3326 0033 | 3771 3800</span>
-				<span>(61) 9976-4074</span>
-				<button type="button" class="btn btn-primary btn-agende">AGENDE <br/>SEU EXAME</button>
-			</div>
-
-			<div class="footer-menu">  		
     			<nav class="f-menu">
 				<ul>
+					<li><a href="#">Exames</a></li>
 					<li><a href="#">Convênios</a></li>
 					<li><a href="#">Resultado de Exames</a></li>
 					<li><a href="#">Unidades</a></li>
@@ -557,39 +546,45 @@ function closeNav() {
 					<li><a href="#">Materiais Gratuitos</a></li>
 				</ul>
 				</nav>
+			</div>
+
+			<div class="footer-menu">  		
+    			<div class="footer-esq">
+					<div class="f-rsociais">
+						<a href="#"><div class="f-ico-fb"></div></a>
+						<a href="#"><div class="f-ico-ig"></div></a>
+						<a href="#"><div class="f-ico-in"></div></a>
+					</div>
+					<h1>CENTRAL DE ATENDIMENTO</h1>
+					<p><img style="margin-right: 10px; width: 19px;" src="imagens/icon-fone.png">(61) 3326 0033 | 3771 3800</p>
+					<p><img style="margin-right: 10px; width: 19px;" src="imagens/icon-wpp.png">(61) 9976-4074</p>
+					<button type="button" class="btn btn-primary btn-agende">AGENDE <br/>SEU EXAME</button>
+				</div>
 				<div class="widgets">
 				<div class="widget-um">
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
+					<a href="#">Ecografia</a>
+					<a href="#">Cintilografia</a>
+					<a href="#">Densitometria Óssea</a>
+					<a href="#">Mamografia e Biópsias</a>
+					<a href="#">PET-CT</a>
+					<a href="#">Ressonâcia Magnética</a>
+					<a href="#">Tomografia Personalizada</a>
+					<a href="#">Suíte Terapêutica</a>
 				</div>
 
 				<div class="widget-dois">
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
+					<a href="#">Aviso de Privacidade Website</a>
+					<a href="#">Aviso de Privacidade - Serviços de Saúde</a>
+					<a href="#">Responsabilidade social</a>
+					<a href="#">Corpo Clínico</a>
+					<a href="#">Trabalhe Conosco</a>
 				</div>
 
 				<div class="widget-tres">
 					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-					<a href="#">Convênios</a>
-				</div>
+					<a href="#">WEBMAIL</a>
+					<a href="#">SISTEMA EPA</a>
+					</div>
 				</div>
 			</div>
     	</div>
@@ -598,56 +593,44 @@ function closeNav() {
     		<h1>ENDEREÇOS</h1>
     		<div class="enderecos">
     			<div class="col-md-3">
-    				<strong>Centro Clínico Sul</strong>
-    					Torre I
-						SHLS 716 conj. L, Centro Clínico Sul Torre I
-						salas T121/124, Brasília- DF – CEP: 70390-700
-						Torre II (Tomografia)
-						SHLS 716 conj. L, Centro Clínico Sul Torre II
-						sala T223, Brasília- DF – CEP: 70390-700
-						Torre II (Medicina Nuclear)
-						SHLS 716 conj. L, Centro Clínico Sul Praça da
-						Saúde sala W162, Brasília- DF – CEP: 70390-700
-						C
+    				<p><strong>Centro Clínico Sul</strong></p>
+    					<p style="margin-bottom: 0"><i>Torre I</i></p>
+						<p style="margin-top: 0">SHLS 716 conj. L, Centro Clínico Sul Torre I
+						salas T121/124, Brasília- DF – CEP: 70390-700</p>
+						<p style="margin-bottom: 0"><i>Torre II (Tomografia)</i></p>
+						<p style="margin-top: 0">SHLS 716 conj. L, Centro Clínico Sul Torre II
+						sala T223, Brasília- DF – CEP: 70390-700</p>
+						<p style="margin-bottom: 0"><i>Torre II (Medicina Nuclear)</i></p>
+						<p style="margin-top: 0">SHLS 716 conj. L, Centro Clínico Sul Praça da
+						Saúde sala W162, Brasília- DF – CEP: 70390-700</p>
     			</div>
     			<div class="col-md-3">
-    				<strong>Centro Clínico Sul</strong>
-    					Torre I
-						SHLS 716 conj. L, Centro Clínico Sul Torre I
-						salas T121/124, Brasília- DF – CEP: 70390-700
-						Torre II (Tomografia)
-						SHLS 716 conj. L, Centro Clínico Sul Torre II
-						sala T223, Brasília- DF – CEP: 70390-700
-						Torre II (Medicina Nuclear)
-						SHLS 716 conj. L, Centro Clínico Sul Praça da
-						Saúde sala W162, Brasília- DF – CEP: 70390-700
-						C
+    				<p><strong>Vitrium Centro Médico</strong></p>
+    				<p>SGAS 915 lote 69a e 70a salas 301 e 303, Brasília- DF – CEP: 70715-900</p>
+    				<p>L2 Sul SGAS Quadra 614 Conjunto C Lote 99 Salas 12 a 15, Brasília – DF CEP: 70200-730</p>
+    				<p style="margin-top: 60px;"><strong>Asa Norte Edifício Dr Crispim</strong></p>
+    				<p>SMHN Quadra 02 Conjunto C Sobreloja 18, Próximo ao Hospital HRAN, Brasília – DF CEP: 70710-100</p>
+
     			</div>
     			<div class="col-md-3">
-    				<strong>Centro Clínico Sul</strong>
-    					Torre I
-						SHLS 716 conj. L, Centro Clínico Sul Torre I
-						salas T121/124, Brasília- DF – CEP: 70390-700
-						Torre II (Tomografia)
-						SHLS 716 conj. L, Centro Clínico Sul Torre II
-						sala T223, Brasília- DF – CEP: 70390-700
-						Torre II (Medicina Nuclear)
-						SHLS 716 conj. L, Centro Clínico Sul Praça da
-						Saúde sala W162, Brasília- DF – CEP: 70390-700
-						C
+    				<p><strong>M Norte</strong></p>
+    				<p>SDE Quadra 1 conjunto A lote 2 CEP:72145-101</p>
+    				<p style="margin-top: 30px"><strong>Taguatinga</strong></p>
+    				<p>Centro de Excelência Anchieta, Área Especial 10, Setor C Norte loja 02. Taguatinga,DF – CEP: 72115-700</p>
+    				<p style="margin-top: 30px"><strong>Edifício Buriti</strong></p>
+    				<p>CLN 116, Bloco H Edifício Buriti loja 33.
+Brasília- DF – CEP: 70773-580</p>
     			</div>
     			<div class="col-md-3">
-    				<strong>Centro Clínico Sul</strong>
-    					Torre I
-						SHLS 716 conj. L, Centro Clínico Sul Torre I
-						salas T121/124, Brasília- DF – CEP: 70390-700
-						Torre II (Tomografia)
-						SHLS 716 conj. L, Centro Clínico Sul Torre II
-						sala T223, Brasília- DF – CEP: 70390-700
-						Torre II (Medicina Nuclear)
-						SHLS 716 conj. L, Centro Clínico Sul Praça da
-						Saúde sala W162, Brasília- DF – CEP: 70390-700
-						C
+    				<p><strong>Centro Clínico Advance</strong></p>
+    				<p>SGAS 915 lote 69a e 70a salas 301 e 303,
+Brasília- DF – CEP: 70715-900</p>
+					<p style="margin-top: 30px"><strong>Gama</strong></p>
+    				<p>Quadra EQ 47-49 Projeção 4,
+St. Central, 2º andar, ao lado
+do Hospital do Gama,
+DF – CEP: 72405-499</p>
+
     			</div>
     		</div>
     	</div>
